d3e7add364b0dd64b7b0030101f708234b9b759c
Ben Bryant <brbryant2639@gmail.com>
2020-08-10 14:27:06 -0500

    Refactored for starter template purposes. Basic starter project with webpack and ts configs, server, and client folders for compiling.

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,23 +1,36 @@
 import * as React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import "./scss/app";
-import List from "./List";
-import Detail from "./Detail";
+class App extends React.Component<IAppProps, IAppState> {
+  constructor(props: IAppProps) {
+    super(props);
+    this.state = {
+      name: null,
+    };
+  }
 
-const App: React.SFC<IAppProps> = props => {
-  return (
-    <main className="container my-5">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={List}></Route>
-          <Route path="/:name" component={Detail}></Route>
-        </Switch>
-      </Router>
-    </main>
-  );
-};
+  async componentDidMount() {
+    try {
+      let r = await fetch("/api/hello");
+      let name = await r.json();
+      this.setState({ name });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-interface IAppProps {}
+  render() {
+    return (
+      <main className="container my-5">
+        <h1 className="text-primary text-center">Hello {this.state.name}!</h1>
+      </main>
+    );
+  }
+}
+
+export interface IAppProps {}
+
+export interface IAppState {
+  name: string;
+}
 
 export default App;
